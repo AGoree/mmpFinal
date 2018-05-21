2440269f39b1ce11fa873070c105f28f43b85cf0
Andrew <abgoree1@gmail.com>
2018-05-21 13:42:19 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home Page</title>
+        <link rel="stylesheet" type="text/css" href="css.css">
+    </head>
+    <body class="bg">
+        
+        <div class="sidenav">
+            
+            <a href="index.php">Home</a>
+            <a href="pages/about.php">About</a>
+            <a href="pages/contact.php">Contact</a>
+            
+
+        </div>
+        <div><img href="images/p2.jpg"></div>
+        <div class="move_logo"><img src="images/p1.png" alt="logo" id="logo"></div>
+        <div class="body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+
+   
+    </body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home Page</title>
+        <link rel="stylesheet" type="text/css" href="../css.css">
+    </head>
+    <body class="bg">
+        
+        <div class="sidenav">
+            
+
+            <a href="../index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+  
+
+        </div>
